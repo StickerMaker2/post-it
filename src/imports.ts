0476a9c84854c1fe5706f098e04caab308f68997
AlexDev404 <AlexDev404@users.noreply.github.com>
2023-02-27 14:37:50 -0600

    Add empty routes + link

--- src/imports.ts
+++ src/imports.ts
@@ -1,12 +1,45 @@
+// Account system
+
 import AAD from "./api/Accounts/Add";
 import AAA from "./api/Accounts/Auth";
 import ADD from "./api/Accounts/Delete";
 import AIE from "./api/Accounts/Invalidate";
 
+// Sticker posting + editing
+
+import STG from "./api/Stickers/Get";
+import STD from "./api/Stickers/Delete";
+import STM from "./api/Stickers/Modify";
+import STP from "./api/Stickers/Post";
+
+// @Me modifications
+import MEG from "./api/Me/Get";
+import MEL from "./api/Me/Library";
+import MEM from "./api/Me/Modify";
+import MEDS from "./api/Me/Delete__SELECTOR";
+
 import express from "express";
 
 const app = express.Router();
 
-app.use([AAD, AAA, ADD, AIE]);
+app.use([
+  // Account system
+  AAD,
+  AAA,
+  ADD,
+  AIE,
+
+  // Sticker posting + editing
+  STG,
+  STD,
+  STM,
+  STP,
+
+  // @Me modifications
+  MEG,
+  MEL,
+  MEM,
+  MEDS, // (lol)
+]);
 
 export = app;
